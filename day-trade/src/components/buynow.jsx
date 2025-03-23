33b66fa4cbc3a5db0b8a15c1d5bef613633c989a
ARTHUR VIEIRA <arthur.vieira2007@hotmail.com>
2025-03-23 15:58:24 -0300

    third commit

--- day-trade/src/components/buynow.jsx
+++ day-trade/src/components/buynow.jsx
@@ -1,14 +1,45 @@
 import './buynow.css'
+import { useState } from 'react'
 
 function BuyNow(){
-const handlerPayment = ()=>{
-    window.location.href = ''
-}
+    const [moused, setMoused] = useState({x: 0, y: 0})
+    const [sizing, setSizing] = useState(false)
+    const [leaving, setLeaving] = useState(false)
+    const [isClipPath, setClipPath] = useState("")
 
+    const handlerPayment = ()=>{
+        window.location.href = ''
+    }
+
+    const handlerMouse = (e)=>{
+        const mainRect = e.currentTarget.getBoundingClientRect()
+        setMoused({
+            x: e.clientX - mainRect.left,
+            y: e.clientY - mainRect.top,
+        })
+    }
+    const handlerLeavingAndRisingMouse = (type) => {
+        if(type === "leave"){
+            return setSizing(false)
+        }
+        if(type === "enter"){
+            return setSizing(true)
+        }
+        if(type === "leave-from-main"){
+            setLeaving(false)
+        }
+        else if(type === "enter-from-main"){
+            setLeaving(true)
+        }
+    }
     return(
         <>
-        <main className='main_payment'>
-            <section className='card'>
+        <main className='main_payment' onMouseMove={(e) => handlerMouse(e)} 
+        onMouseOut={() => handlerLeavingAndRisingMouse('leave-from-main')}
+        onMouseOver={() => handlerLeavingAndRisingMouse('enter-from-main')}>
+            <section className='card' 
+            onMouseLeave={() => handlerLeavingAndRisingMouse('enter')}
+            onMouseEnter={() => handlerLeavingAndRisingMouse('leave')}>
                 <h2><span class="material-symbols-outlined">
                     check
                 </span>Conhecimento em Day Trading</h2>
@@ -20,11 +51,23 @@ const handlerPayment = ()=>{
                 </span>Conhecimento em Como vender as Ações na hora exata</h2>
                 <h2><span class="material-symbols-outlined">
                     check
-                </span>Ficvar Bom nisso e viver disso</h2>
+                </span>Ficar Bom nisso e viver disso</h2>
             </section>
-            <section className='card_payment'>
-                <button onClick={handlerPayment}>Pagar Agora</button>
+            <section className='card_payment'
+                onMouseLeave={() => handlerLeavingAndRisingMouse('enter')}
+                onMouseEnter={() => handlerLeavingAndRisingMouse('leave')}>
+                <button onClick={handlerPayment} className='button_payment'>Pagar Agora</button>
             </section>
+            <div className='cursor'
+                style={{
+                top: `${moused.y}px`,
+                left: `${moused.x}px`,
+                width: sizing ? "15px" : "80px",
+                height: sizing ? "15px" : "80px",
+                transition: "linear 0.1s 0.001s",
+                display: leaving ? "block" : "none"
+                }}
+                />
         </main>
         </>
     )
--- day-trade/src/components/description.jsx
+++ day-trade/src/components/description.jsx
@@ -1,10 +1,11 @@
 import classes from './description.module.css'
 import CandleStickImg from './../assets/second-img.jpg'
-import { useState} from 'react';
+import { useState } from 'react';
 
 function Description(){
     const [moused, setMoused] = useState({x: 0, y: 0})
     const [sizing, setSizing] = useState(false)
+    const [leaving, setLeaving] = useState(false)
 
     const handlerMouse = (e)=>{
         const mainRect = e.currentTarget.getBoundingClientRect()
@@ -20,11 +21,19 @@ function Description(){
         if(type === "rise"){
             return setSizing(true)
         }
+        if(type === "leave-from-main"){
+            setLeaving(false)
+        }
+        else if(type === "enter-from-main"){
+            setLeaving(true)
+        }
     }
 
     return(
         <>
-            <main className={classes.main_description} onMouseMove={(e) => handlerMouse(e)}>
+            <main className={classes.main_description} onMouseMove={(e) => handlerMouse(e)}
+                onMouseOut={() => handlerLeavingAndRisingMouse('leave-from-main')}
+                onMouseOver={() => handlerLeavingAndRisingMouse('enter-from-main')}>
                 <img src={CandleStickImg} 
                 onMouseLeave={() => handlerLeavingAndRisingMouse('rise')}
                 onMouseEnter={() => handlerLeavingAndRisingMouse('leave')}/>
@@ -40,7 +49,8 @@ function Description(){
                     left: `${moused.x}px`,
                     width: sizing ? "15px" : "80px",
                     height: sizing ? "15px" : "80px",
-                    transition: "linear 0.1s 0.001s"
+                    transition: "linear 0.1s 0.001s",
+                    display: leaving ? "block" : "none"
                     }}
                 />
             </main>
